f0b28cf4ee9f83fcb7151f07824f1f434f2117ee
ajupowicz <aleksandra.jupowicz@gmail.com>
2026-05-20 16:24:23 +0200

    Revert "Merge branch 'interactions'"
    This reverts commit a039dbe4c8c4bfc6d4202be4cb302bf5bbd1ffd5, reversing
    changes made to d42830f8a9b5ec952ab540c7c9a724d8a63f222a.

--- Assets/Scripts/ExhbitInfo.cs
+++ Assets/Scripts/ExhbitInfo.cs
@@ -6,37 +6,29 @@ public class ExhibitInfo : MonoBehaviour
     [Header("Exhibit Information")]
     public string title = "Artwork Title";
     public string author = "Author / Artist";
-    
+
     // The TextArea attribute makes the text box larger in the Unity Inspector
     [TextArea(3, 10)]
     public string description = "Enter the exhibit description here...";
 
     [Header("UI Plaque Connections")]
     [Tooltip("Drag TextMeshPro objects from your scene here")]
-    public bool visible = true;
     public TMP_Text titleUIText;
     public TMP_Text authorUIText;
     public TMP_Text descriptionUIText;
 
     void Start()
     {
-        // This function runs automatically when the game starts 
-        UpdatePlaque(); // Initialize the plaque with the current exhibit information
+        // This function runs automatically when the game starts
+        UpdatePlaque();
     }
 
     // Separate function to update texts (useful for potential future features
     // like changing the language during gameplay)
     public void UpdatePlaque()
     {
-        this.visible = !this.visible;
-        if (this.visible == true){
-            if (this.titleUIText != null) titleUIText.text = title;
-            if (this.authorUIText != null) authorUIText.text = author;
-            if (this.descriptionUIText != null) descriptionUIText.text = description;
-        }
-        else{        
-        this.titleUIText.text = "";
-        this.authorUIText.text = "";
-        this.descriptionUIText.text = "";}
+        if (titleUIText != null) titleUIText.text = title;
+        if (authorUIText != null) authorUIText.text = author;
+        if (descriptionUIText != null) descriptionUIText.text = description;
     }
 }
